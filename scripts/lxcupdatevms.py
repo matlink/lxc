44ec0d3f6a81c75b0e2ba37cfe9687941c53bc1d
matlink <matlink@matlink.fr>
2015-08-02 00:13:03 +0200

    [fix] can disable threads

--- scripts/lxcupdatevms.py
+++ scripts/lxcupdatevms.py
@@ -10,6 +10,7 @@ ENDC = '\033[0m'
 BOLD = '\033[1m'
 UNDERLINE = '\033[4m'
 
+enable_threads = False
 updating = list()
 to_update = list(lxc.list_containers(as_object=True))
 timing = dict()
@@ -39,21 +40,33 @@ def update(container):
     if started:
         if not container.shutdown(30):
             container.stop()
-    updating.remove(container)
+    if threaded:
+        updating.remove(container)
 
-while len(to_update) > 0:
-    if len(updating) <= 5:
-        container = to_update.pop(0)
-        updating.append(container)
-        t = threading.Thread(target=update, args=(container,))
-        t.start()
-        timing[container.name] = time.time()
+def serial():
+    for container in to_update:
+        update(container)
+
+def threaded():
+    while len(to_update) < 0:
+        if len(updating) == 0:
+            container = to_update.pop(0)
+            updating.append(container)
+            t = threading.Thread(target=update, args=(container,))
+            t.start()
+            timing[container.name] = time.time()
+        else:
+            for container in updating:
+                current_time = time.time()
+                if timing[container.name]-current_time >= 90:
+                    print(FAIL,'cannot update',container.name,ENDC)
+                    container.stop()
+                    updating.remove(container)
+            print(OKBLUE,'updating ...', [container.name for container in updating],ENDC)
+            time.sleep(5)
+if __name__=='__main__':
+    if enable_threads:
+        threaded()
     else:
-        for container in updating:
-            current_time = time.time()
-            if timing[container.name]-current_time >= 90:
-                print(FAIL,'cannot update',container.name,ENDC)
-                container.stop()
-                updating.remove(container)
-        print(OKBLUE,'updating ...', [container.name for container in updating],ENDC)
-        time.sleep(5)
+        serial()
+
